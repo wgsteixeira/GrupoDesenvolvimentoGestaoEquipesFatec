303a4fdad7c0671329282aa790ca2e5cea5603ab
wgsteixeira <wgsteixeira@outlook.com>
2023-05-09 11:34:40 -0300

    AB#20

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import styles from './Footer.css'
+
+function Footer() {
+    return (
+        <footer id='footer' className='footer-wrapper'>
+            <p className='footer-p'>
+                TotalExpress
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
