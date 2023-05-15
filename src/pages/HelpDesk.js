b11219ec2809a47ae9ae59b4219aba9b19e6c29b
Wellington Teixeira <wgsteixeira@outlook.com>
2023-05-15 12:13:55 -0300

    AB#20

---
+++ src/pages/HelpDesk.js
@@ -0,0 +1,25 @@
+import './HelpDesk.css'
+
+function HelpDesk() {
+    return (
+        <div>
+            <h1>Help Desk</h1>
+            <p>Bem vindo a central de ajuda</p>
+            <input placeholder="Encontrar ajuda e serviços"></input>
+            <div>
+                <p>Forum de Duvidas</p>
+                <p>Suporte</p>
+            </div>
+
+            <div>
+                <p>ENTRE EM CONTATO</p>
+                <p>(04) 298 3985 2092</p>
+                <p>+76 209 1092 4095</p>
+                <p>contato@totalexpress.com.br</p>
+            </div>
+        </div>
+        
+    )
+}
+
+export default HelpDesk;
