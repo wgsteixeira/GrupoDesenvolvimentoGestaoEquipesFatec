9adb8aefe6ab8f68ec5496f223c4b8698010119d
Wellington Teixeira <wgsteixeira@outlook.com>
2023-05-15 08:45:17 -0300

    AB#20

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,34 @@
-import styles from './Footer.css'
+import React from 'react';
+import './Footer.css';
 
-function Footer() {
+const Footer = () => {
     return (
-        <footer id='footer' className='footer-wrapper'>
-            <p className='footer-p'>
-                TotalExpress
-            </p>
-        </footer>
-    )
-}
+        <div className="footer">
+            <div className="row">
+                <div className="column">
+                    <h3>Sobre Nós</h3>
+                    <ul>
+                        <li>Vagas abertas</li>
+                        <li>Política de recrutamento</li>
+                    </ul>
+                </div>
+                <div className="column">
+                    <h3>Projetos de Destaque</h3>
+                    <ul>
+                        <li><a href='#'>Projeto 01</a></li>
+                        <li><a href='#'>Projeto 02</a></li>
+                    </ul>
+                </div>
+                <div className="column">
+                    <h3>TotalExpress</h3>
+                    <ul>
+                        <li>Contato: contato@totalexpress.com.br</li>
+                        <li>Telefone: (18) 3916-7887</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Footer
+export default Footer;
