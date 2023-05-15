cb5130228719ad75843f094974f022f1ddc70f1d
Wellington Teixeira <wgsteixeira@outlook.com>
2023-05-15 12:15:45 -0300

    Fix: Import Components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Footer from './components/layout/Footer';
+import Footer from './layout/Footer';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
