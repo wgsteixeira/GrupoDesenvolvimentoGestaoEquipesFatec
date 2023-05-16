54d57826084502e71651232ae6d768e24e242de7
wgsteixeira <wgsteixeira@outlook.com>
2023-05-16 10:37:04 -0300

    AB#20

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import App from './App';
 import Footer from './layout/Footer';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
+import Navbar from './layout/Navbar';
+import HelpDesk from './pages/HelpDesk';
+import Portfolio from './pages/Portfolio';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Navbar />
     <App />
-    <Footer/>
+    <Portfolio />
+    <HelpDesk />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
