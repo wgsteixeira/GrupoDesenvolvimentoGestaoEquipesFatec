2ea6dd0722d5d73208b6503db42dca13861d38c9
Wellington Teixeira <wgsteixeira@outlook.com>
2023-05-20 22:29:34 -0300

    AB#20

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import './Footer.css';
 
@@ -6,25 +7,25 @@ const Footer = () => {
         <div className="footer">
             <div className="row">
                 <div className="column">
-                    <h3>Sobre Nós</h3>
-                    <ul>
+                    <div className='title'>Sobre Nós</div>
+                    <div className='list_itens'>
                         <li>Vagas abertas</li>
                         <li>Política de recrutamento</li>
-                    </ul>
+                    </div>
                 </div>
                 <div className="column">
-                    <h3>Projetos de Destaque</h3>
-                    <ul>
-                        <li><a href='#'>Projeto 01</a></li>
-                        <li><a href='#'>Projeto 02</a></li>
-                    </ul>
+                    <div className='title'>Projetos de Destaque</div>
+                    <div className='list_itens'>
+                        <li><a href='/'>Projeto 01</a></li>
+                        <li><a href='/'>Projeto 02</a></li>
+                    </div>
                 </div>
                 <div className="column">
-                    <h3>TotalExpress</h3>
-                    <ul>
+                    <div className='title'>TotalExpress</div>
+                    <div className='list_itens'>
                         <li>Contato: contato@totalexpress.com.br</li>
                         <li>Telefone: (18) 3916-7887</li>
-                    </ul>
+                    </div>
                 </div>
             </div>
         </div>
