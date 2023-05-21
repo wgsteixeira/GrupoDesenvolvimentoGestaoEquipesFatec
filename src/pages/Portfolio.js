569a770a5661ad502a8d1c3eeb71c235c69d8426
Wellington Teixeira <wgsteixeira@outlook.com>
2023-05-20 22:28:18 -0300

    AB#30

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,29 +1,8 @@
+import './Portfolio.css'
+
 function Portfolio() {
     return (
-        <div>
-            <div class="helpdesk-container">
-                <div class="search-container">
-                    <h1>Help desk</h1>
-                    <p>Bem vindo a central de ajuda</p>
-                    <input placeholder="Encontrar ajuda e serviços"></input>
-                    <div class="box-container-container">
-
-                        <div class='box-container'>
-                            <div className='text-div'>Forum de Duvidas</div>
-                        </div>
-                        <div class='box-container'>
-                            <div className='text-div'>Suporte</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class='sb-box-container'>
-                <h2>ENTRE EM CONTATO</h2>
-                <p>(04) 298 3985 2092</p>
-                <p>+76 209 1092 4095</p>
-                <p>contato@totalexpress.com.br</p>
-            </div>
-        </div>
+        <div id="portfolio" className="container">Portfolio</div>
     )
 }
 
