26838089c79fff363e7185f0ab9a73f680743cca
wgsteixeira <wgsteixeira@outlook.com>
2023-05-23 11:58:22 -0300

    AB#30

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,8 +1,19 @@
 import './Portfolio.css'
+import document from '../img/document.png'
 
 function Portfolio() {
     return (
-        <div id="portfolio" className="container">Portfolio</div>
+        <div id="portfolio" className="container">
+            <div className='carrossel'>
+                <div className='item'>
+                    <div className='logo-document'>
+                        <img src={document} />
+                    </div>
+                    <span className='name'>Abertura de chamados</span>
+                    <span className='description'>akjsdgvkiashgrdfouaesbfiuhwuioashrfoiaewharopifjh</span>
+                </div>
+            </div>
+        </div>
     )
 }
 
