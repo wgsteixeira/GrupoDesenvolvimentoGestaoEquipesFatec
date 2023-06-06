66286735b4d718309b1d60f9c5146cda3bec00a5
CD <102435352+carlosdaguano@users.noreply.github.com>
2023-06-06 10:32:59 -0300

    AB#15

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -6,15 +6,15 @@ import logo from '../img/logo.png'
 const Navbar = () => {
     return (
         <header class="navbar">
-            <div class="navbar-header">
+            <div class=" navbar-header">
                 <img class="logo-image" src={logo} alt="imagem" />
             </div>
-            <div class="navbar-menu">
-                <a class="item home-button" href="#home">Home</a>
-                <a class="item portfolio-button" href="#portfolio">Portfólio</a>
-                <a class="item helpdesk-button" href="#helpdesk">Help Desk</a>
+            <div class=" navbar-menu">
+                <a class="item-nav home-button" href="#home">Home</a>
+                <a class="item-nav portfolio-button" href="#portfolio">Portfólio</a>
+                <a class="item-nav helpdesk-button" href="#helpdesk">Help Desk</a>
             </div>
-            <div class="navbar-direita"><button class="login-button">Login</button></div>
+            <div class=" navbar-direita"><button class="login-button">Login</button></div>
         </header>
     );
 }
