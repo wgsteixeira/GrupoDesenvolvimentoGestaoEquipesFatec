ba359e203c935f466f9fd7288fe54bc846ffa365
CD <102435352+carlosdaguano@users.noreply.github.com>
2023-06-06 11:51:53 -0300

    AB#15

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 import './Navbar.css';
 import logo from '../img/logo.png'
+import { Modal } from 'react-overlays';
 
 
 const Navbar = () => {
+    // React state to control Modal visibility
+    const [showModal, setShowModal] = useState(false);
+
+    // Backdrop JSX code
+    const renderBackdrop = (props) => <div className="backdrop" {...props} />;
+
+    var handleClose = () => setShowModal(false);
+
+    var handleSuccess = () => {
+        console.log("success");
+    };
     return (
         <header class="navbar">
             <div class=" navbar-header">
@@ -14,7 +27,35 @@ const Navbar = () => {
                 <a class="item-nav portfolio-button" href="#portfolio">Portfólio</a>
                 <a class="item-nav helpdesk-button" href="#helpdesk">Help Desk</a>
             </div>
-            <div class=" navbar-direita"><button class="login-button">Login</button></div>
+            <div class=" navbar-direita"><button id="login-button" class="login-button" onClick={() => setShowModal(true)}>Login</button></div>
+            <Modal className="modal"
+                show={showModal}
+                onHide={handleClose}
+                renderBackdrop={renderBackdrop}>
+                <div>
+                    <div className="modal-header">
+                        <div className="modal-title">LOGIN DO USUÁRIO</div>
+                        <div>
+                            <span className="close-button" onClick={handleClose}>
+                                x
+                            </span>
+                        </div>
+                    </div>
+                    <div className="modal-desc">
+                        <form>
+                            <label for="email">E-mail</label><br></br>
+                            <input type="text" placeholder='Digite o E-mail'></input><br></br>
+                            <label for="senha">Senha</label><br></br>
+                            <input type="text" placeholder='Digite a Senha'></input><br></br>
+                            <input type="submit" value="Login"></input><br></br>
+                            <input type="button" onClick={handleClose} value="Cancelar"></input><br></br>
+                            <p><input type="checkbox"></input>Lembrar Senha</p>
+                            <a href="#">Esqueceu a senha?</a><br></br>
+                            <p>Não está registrado? <a href="#">Registrar</a></p>
+                        </form>
+                    </div>
+                </div>
+            </Modal>
         </header>
     );
 }
