d8020dabee3e171d80892a1e59f30bd93be602b9
Wellington Teixeira <wgsteixeira@outlook.com>
2023-06-08 12:58:09 -0300

    AB#30

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,17 +1,50 @@
+import { useEffect, useState, useRef } from 'react';
 import './Portfolio.css'
-import document from '../img/document.png'
 
 function Portfolio() {
+
+    const [data, setData] = useState([]);
+
+    const carousel = useRef(null);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/static/shoes.json').then((response) => response.json()).then(setData);
+    }, []);
+
+    const hadleLeftClick = (e) => {
+        e.preventDefault();
+        console.log(carousel.current.offsetWidth);
+        carousel.current.scrollLeft -= carousel.current
+    }
+
+    const hadleRightClick = (e) => {
+        e.preventDefault();
+        console.log(carousel.current.offsetWidth);
+        carousel.current.scrollLeft += carousel.current.offsetWidth;
+    }
+
+    if (!data || !data.length) return null;
     return (
-        <div id="portfolio" className="container">
-            <div className='carrossel'>
-                <div className='item'>
-                    <div className='logo-document'>
-                        <img src={document} alt=''/>
-                    </div>
-                    <span className='name'>Abertura de chamados</span>
-                    <span className='description'>akjsdgvkiashgrdfouaesbfiuhwuioashrfoiaewharopifjh</span>
-                </div>
+        <div className="container_portfolio" id="portfolio">
+            <div className='carousel_portfolio' ref={carousel}>
+                {data.map((item) => {
+                    const { id, name, description, image } = item;
+                    return (
+                        <div className='item_portfolio' key={id} >
+                            <div className='image_portfolio'>
+                                <img src={image} alt={image} />
+                            </div>
+                            <div className='info_portfolio'>
+                                <span className='name_portfolio'>{name}</span>
+                                <span className='description'>{description}</span>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+            <div className='buttons_portfolio'>
+                <button onClick={hadleLeftClick}><img src='/static/images/216151_right_chevron_icon.png' alt='Scroll Left' /></button>
+                <button onClick={hadleRightClick}><img src='/static/images/216151_right_chevron_icon.png' alt='Scroll Right' /></button>
             </div>
         </div>
     )
